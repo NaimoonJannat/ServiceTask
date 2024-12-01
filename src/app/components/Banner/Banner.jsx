ff6977326a9237e416c1884d62265cff15f69c3f
NaimoonJannat <naimoonjannat@gmail.com>
2024-12-01 13:01:53 +0600

    first commit

---
+++ src/app/components/Banner/Banner.jsx
@@ -0,0 +1,6 @@
+// components/Banner.js
+import styles from './Banner.module.scss';
+
+export default function Banner() {
+  return <div className={styles.banner}>Welcome to My Next.js App!</div>;
+}
